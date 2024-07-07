c27a58702cb6e3191a64a8970b16f835a79ede23
Abdulaziz718 <githubgaemail@gmail.com>
2024-07-07 12:58:06 +0500

    created developer environment 7/7/24

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+
+
+const App = () => {
+  return (
+    <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        style={{ zIndex: 10000 }}
+      />
+    </div>
+  )
+}
+
+export default App
